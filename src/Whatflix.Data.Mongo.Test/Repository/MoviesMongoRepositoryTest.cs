72d689dd55e421fa3ace77bcb58e9761eb860a18
Nikita.bm <nikitabmathew@gmail.com>
2019-05-01 19:54:56 +0530

    MoviesMongoRepositoryTest updated.

--- src/Whatflix.Data.Mongo.Test/Repository/MoviesMongoRepositoryTest.cs
+++ src/Whatflix.Data.Mongo.Test/Repository/MoviesMongoRepositoryTest.cs
@@ -19,12 +19,14 @@ namespace Whatflix.Data.Mongo.Test.Repository
         private IOptions<SettingsWrapper> _mockServieSettings;
         private Mock<IMapper> _mockMapper;
 
+
         [TestInitialize]
         public void InitializeTest()
         {
             _mockServieSettings = CreateSettings();
             _mockMapper = new Mock<IMapper>();
             _movieRepository = new MoviesMongoRepository(_mockServieSettings, _mockMapper.Object);
+
         }
 
         [TestMethod]
@@ -38,6 +40,97 @@ namespace Whatflix.Data.Mongo.Test.Repository
             {
                 // Act
                 await _movieRepository.InsertMany(It.IsAny<IEnumerable<IMovieEntity>>());
+
+                // Assert
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                // Assert
+                Assert.IsInstanceOfType(ex, expectedErrorType);
+                Assert.AreEqual(expectedMessage, ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public async Task GetRecommendations_InvalidParameter_ThrowsArgumentNullException()
+        {
+            // Arrange
+            var expectedErrorType = typeof(ArgumentNullException);
+            var expectedMessage = "Value cannot be null.\r\nParameter name: values";
+
+            try
+            {
+                // Act
+                await _movieRepository.GetRecommendationsAsync(It.IsAny<List<string>>(), It.IsAny<List<string>>(), It.IsAny<List<string>>());
+
+                // Assert
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                // Assert
+                Assert.IsInstanceOfType(ex, expectedErrorType);
+                Assert.AreEqual(expectedMessage, ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public async Task Search_InvalidParameters_ThrowsArgumentNullException()
+        {
+            // Arrange
+            var expectedErrorType = typeof(ArgumentNullException);
+            var expectedMessage = "Value cannot be null.\r\nParameter name: searchWords";
+
+            try
+            {
+                // Act
+                await _movieRepository.SearchAsync(It.IsAny<string[]>(),It.IsAny<List<string>>(), It.IsAny<List<string>>(), It.IsAny<List<string>>());
+
+                // Assert
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                // Assert
+                Assert.IsInstanceOfType(ex, expectedErrorType);
+                Assert.AreEqual(expectedMessage, ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public async Task Search_InvalidSearchWords_ThrowsArgumentNullException()
+        {
+            // Arrange
+            var expectedErrorType = typeof(ArgumentNullException);
+            var expectedMessage = "Value cannot be null.\r\nParameter name: searchWords";
+
+            try
+            {
+                // Act
+                await _movieRepository.SearchAsync(It.IsAny<string[]>());
+
+                // Assert
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                // Assert
+                Assert.IsInstanceOfType(ex, expectedErrorType);
+                Assert.AreEqual(expectedMessage, ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public async Task Update_InvalidParameters_ThrowsArgumentNullException()
+        {
+            // Arrange
+            var expectedErrorType = typeof(ArgumentNullException);
+            var expectedMessage = "Value cannot be null.\r\nParameter name: movieIds";
+            try
+            {
+                // Act
+                await _movieRepository.UpdateAppeardInSearchAsync(It.IsAny<List<int>>());
 
                 // Assert
                 Assert.Fail();
